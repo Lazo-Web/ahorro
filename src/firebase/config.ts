66f5c6619060345580a8ede3034432a0a8349c57
Horacio Lazo Torres <Horaciolazo@gmail.com>
2025-12-10 14:59:42 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-6441500097-cf26b",
+  "appId": "1:681089828988:web:f7af7e742ccb326ae721f9",
+  "apiKey": "AIzaSyAsXwN9rRsibeXeB8u-P2_og9MPH9d3jjo",
+  "authDomain": "studio-6441500097-cf26b.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "681089828988"
+};
